423aa447c0117db8bf57ed3a4b4a863dd283947d
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-10-29 10:20:33 -0600

    Update ruleta.py

--- ruleta.py
+++ ruleta.py
@@ -5,3 +5,10 @@ numeros = range(0, 37)
 
 numero_usuario = int (input("Elige un numero: "))
 color_usuario = input("Elige un color: ")
+
+if numero_usuario not in numeros or
+color_usuario.lower() not in colores:
+  print("ERROR, DATOS NO VALIDOS")
+  quit()
+
+color_ganador = random.choice
