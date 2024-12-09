7816f5ec76d9c93af4f967236bd59e30e42aee2b
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-12-08 22:56:27 -0600

    Update ruleta.py

--- ruleta.py
+++ ruleta.py
@@ -6,6 +6,7 @@ numeros = range(0, 37)
 numero_usuario = int (input("Elige un numero: ")) 
 color_usuario = input("Elige un color: ")
 
+
 if numero_usuario not in numeros or
 color_usuario.lower() not in colores:
   print("ERROR, DATOS NO VALIDOS")
