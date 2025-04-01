71ca7085b904e0d176a2a732d5207828627fd1cc
Angel Zarco <91713291+ZarcoVM@users.noreply.github.com>
2025-04-01 06:18:01 -0600

    Actualizar ruleta.py

--- ruleta.py
+++ ruleta.py
@@ -2,6 +2,7 @@ import random
 colores = ["rojo", "negro"]
 numeros = range(0, 37)
 
+
 numero_usuario = int (input("Elige un numero: ")) 
 color_usuario = input("Elige un color: ")
 if numero_usuario not in numeros or
